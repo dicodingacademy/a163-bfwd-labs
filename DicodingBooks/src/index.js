0d5a721ac99bbfd19b886ec65ede7a3f0804255f
Dimas <dimas.maulanads@gmail.com>
2020-03-16 10:49:31 +0700

    Initial project starter

---
+++ DicodingBooks/src/index.js
@@ -0,0 +1,10 @@
+/*
+        jangan ubah kode di bawah ini ya!
+*/
+
+import "regenerator-runtime";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/main.css";
+import main from "./scripts/main";
+
+main();
