06be24f3c32ddde8f65c179302c554392a53b09c
Dimas <dimas.maulanads@gmail.com>
2020-03-16 08:32:50 +0700

    Solution for Destructuring Object

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -11,9 +11,7 @@ const main = function () {
     const renderResult = function (results) {
         clubListElement.innerHTML = "";
         results.forEach(function (club) {
-            const name = club.name;
-            const fanArt = club.fanArt;
-            const description = club.description;
+            const {name, fanArt, description} = club;
 
             const clubElement = document.createElement("div");
             clubElement.setAttribute("class", "club");
