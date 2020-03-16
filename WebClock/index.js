06c33eb520f37f4f911c47b9f3b2b1d63478b302
Dimas <dimas.maulanads@gmail.com>
2020-03-16 09:49:34 +0700

    using installed package

---
+++ WebClock/index.js
@@ -0,0 +1,12 @@
+const displayTime = () => {
+    moment.locale("id");
+    $(".time").text(moment().format("LTS"));
+    $(".date").text(moment().format("LL"));
+};
+
+const updateTime = () => {
+    displayTime();
+    setTimeout(updateTime, 1000)
+};
+
+updateTime();
