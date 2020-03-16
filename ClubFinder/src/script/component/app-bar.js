7525e9c0faf45a20b6f0ba47ccc3258e0b77e7b1
Dimas <dimas.maulanads@gmail.com>
2020-03-16 09:27:36 +0700

    Solution for app-bar

--- ClubFinder/src/script/component/app-bar.js
+++ ClubFinder/src/script/component/app-bar.js
@@ -1,10 +1,34 @@
 class AppBar extends HTMLElement {
+
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
     connectedCallback(){
         this.render();
     }
 
     render() {
-        this.innerHTML = `<h2>Club Finder</h2>`;
+        this.shadowDOM.innerHTML = `
+       <style>
+           * {
+               margin: 0;
+               padding: 0;
+               box-sizing: border-box;
+           }
+           :host {
+               display: block;
+               width: 100%;
+               background-color: cornflowerblue;
+               color: white;
+               box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+           }
+           h2 {
+               padding: 16px;
+           }
+       </style>
+       <h2>Club Finder</h2>`;
     }
 }
 
