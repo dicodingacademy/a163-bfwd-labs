66183907198b286588ee229f2c43f7ea937f3e47
Dimas <dimas.maulanads@gmail.com>
2020-03-16 08:46:41 +0700

    Solution for Class

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,14 +1,16 @@
-function DataSource(onSuccess, onFailed) {
-    this.onSuccess = onSuccess;
-    this.onFailed = onFailed;
-}
-
-DataSource.prototype.searchClub = function (keyword) {
-    const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-    if (filteredClubs.length) {
-        this.onSuccess(filteredClubs);
-    } else {
-        this.onFailed(`${keyword} is not found`);
+class DataSource {
+    constructor(onSuccess, onFailed) {
+        this.onSuccess = onSuccess;
+        this.onFailed = onFailed;
     }
-};
+
+    searchClub(keyword) {
+        const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+        if (filteredClubs.length) {
+            this.onSuccess(filteredClubs);
+        } else {
+            this.onFailed(`${keyword} is not found"`);
+        }
+    }
+}
