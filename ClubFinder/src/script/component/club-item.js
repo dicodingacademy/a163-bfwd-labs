a76590e435ec5988991190093f56e214d5117dc9
Dimas <dimas.maulanads@gmail.com>
2020-03-16 09:35:09 +0700

    Solution for club-list and club-item

--- ClubFinder/src/script/component/club-item.js
+++ ClubFinder/src/script/component/club-item.js
@@ -1,12 +1,57 @@
 class ClubItem extends HTMLElement {
+
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
     set club(club) {
         this._club = club;
         this.render();
     }
 
     render() {
-        this.innerHTML = `
-            <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
+        this.shadowDOM.innerHTML = `
+           <style>
+              * {
+                   margin: 0;
+                   padding: 0;
+                   box-sizing: border-box;
+               }
+               :host {
+                   display: block;
+                   margin-bottom: 18px;
+                   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                   border-radius: 10px;
+                   overflow: hidden;
+               }
+              
+               .fan-art-club {
+                   width: 100%;
+                   max-height: 300px;
+                   object-fit: cover;
+                   object-position: center;
+               }
+              
+               .club-info {
+                   padding: 24px;
+               }
+              
+               .club-info > h2 {
+                   font-weight: lighter;
+               }
+              
+               .club-info > p {
+                   margin-top: 10px;
+                   overflow: hidden;
+                   text-overflow: ellipsis;
+                   display: -webkit-box;
+                   -webkit-box-orient: vertical;
+                   -webkit-line-clamp: 10; /* number of lines to show */
+               }
+
+           </style>
+           <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
            <div class="club-info">
                <h2>${this._club.name}</h2>
                <p>${this._club.description}</p>
--- ClubFinder/src/script/component/club-list.js
+++ ClubFinder/src/script/component/club-list.js
@@ -1,23 +1,37 @@
 import './club-item.js';
 
 class ClubList extends HTMLElement {
+
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
     set clubs(clubs) {
         this._clubs = clubs;
         this.render();
     }
 
     render() {
-        this.innerHTML = "";
+        this.shadowDOM.innerHTML = "";
         this._clubs.forEach(club => {
             const clubItemElement = document.createElement("club-item");
             clubItemElement.club = club;
-            this.appendChild(clubItemElement);
+            this.shadowDOM.appendChild(clubItemElement);
         })
     }
 
     renderError(message) {
-        this.innerHTML = "";
-        this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+        this.shadowDOM.innerHTML = `
+         .placeholder {
+               font-weight: lighter;
+               color: rgba(0,0,0,0.5);
+               -webkit-user-select: none;
+               -moz-user-select: none;
+               -ms-user-select: none;
+               user-select: none;
+           }`;
+        this.shadowDOM.innerHTML += `<h2 class="placeholder">${message}</h2>`;
     }
 }
 
