e7ff6dddd40f8562c8ae3527da5ea2eb7fa2cfff
Dimas <dimas.maulanads@gmail.com>
2020-03-16 08:20:18 +0700

    Initial Club Finder Starter Project

---
+++ ClubFinder/src/script/data/data-source.js
@@ -0,0 +1,16 @@
+function DataSource(onSuccess, onFailed) {
+    this.onSuccess = onSuccess;
+    this.onFailed = onFailed;
+}
+
+DataSource.prototype.searchClub = function (keyword) {
+    var filteredClubs = clubs.filter(function (club) {
+        return club.name.toUpperCase().includes(keyword.toUpperCase());
+    });
+
+    if (filteredClubs.length) {
+        this.onSuccess(filteredClubs);
+    } else {
+        this.onFailed(keyword + " is not found");
+    }
+};
---
+++ ClubFinder/src/script/view/main.js
@@ -0,0 +1,36 @@
+var main = function () {
+    var searchElement = document.querySelector("#searchElement");
+    var buttonSearchElement = document.querySelector("#searchButtonElement");
+    var clubListElement = document.querySelector("#clubList");
+
+    var onButtonSearchClicked = function () {
+        var dataSource = new DataSource(renderResult, fallbackResult);
+        dataSource.searchClub(searchElement.value);
+    };
+
+    var renderResult = function (results) {
+        clubListElement.innerHTML = "";
+        results.forEach(function (club) {
+            var name = club.name;
+            var fanArt = club.fanArt;
+            var description = club.description;
+
+            var clubElement = document.createElement("div");
+            clubElement.setAttribute("class", "club");
+
+            clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
+                '<div class="club-info">\n' +
+                '<h2>' + name + '</h2>\n' +
+                '<p>' + description + '</p>' +
+                '</div>';
+            clubListElement.appendChild(clubElement);
+        })
+    };
+
+    var fallbackResult = function (message) {
+        clubListElement.innerHTML = "";
+        clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
+    };
+
+    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+};
