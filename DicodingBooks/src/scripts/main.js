7499c9a649e45ae758de34aa822d1003270659c5
Dimas <dimas.maulanads@gmail.com>
2020-03-16 11:35:03 +0700

    implementing XHR

--- DicodingBooks/src/scripts/main.js
+++ DicodingBooks/src/scripts/main.js
@@ -1,20 +1,108 @@
 function main() {
 
+    const baseUrl = "https://web-server-book-dicoding.appspot.com";
+
     const getBook = () => {
-        // tuliskan kode di sini!
+        // membuat instance dari XMLHttpRequest
+        const xhr = new XMLHttpRequest();
+
+        //menetapkan callback jika response sukses dan error
+        xhr.onload = function() {
+            const responseJson = JSON.parse(this.responseText);
+            if(responseJson.error) {
+                showResponseMessage(responseJson.message);
+            } else {
+                renderAllBooks(responseJson.books);
+            }
+        };
+
+        xhr.onerror = function() {
+            showResponseMessage();
+        };
+
+        // Membuat GET request dan menetapkan target URL
+        xhr.open("GET", `${baseUrl}/list`);
+        // Mengirimkan request
+        xhr.send();
     };
 
 
     const insertBook = (book) => {
-        // tuliskan kode di sini!
+        // Membuat instance dari XMLHttpRequest
+        const xhr = new XMLHttpRequest();
+
+        //menetapkan callback jika response sukses dan error
+        xhr.onload = function() {
+            const responseJson = JSON.parse(this.responseText);
+            console.log(responseJson);
+            showResponseMessage(responseJson.message);
+            getBook();
+        };
+
+        xhr.onerror = function() {
+            showResponseMessage();
+        };
+
+        // Membuat POST request dan menetapkan target URL
+        xhr.open("POST", `${baseUrl}/add`);
+
+        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("X-Auth-Token", "12345");
+
+        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
+        xhr.send(JSON.stringify(book));
     };
 
     const updateBook = (book) => {
-        // tuliskan kode di sini!
+        // Membuat instance dari XMLHttpRequest
+        const xhr = new XMLHttpRequest();
+
+        //menetapkan callback jika response sukses dan error
+        xhr.onload = function() {
+            const responseJson = JSON.parse(this.responseText);
+            showResponseMessage(responseJson.message);
+            getBook();
+        };
+
+        xhr.onerror = function() {
+            showResponseMessage();
+        };
+
+        // Membuat PUT request dan menetapkan target URL
+        xhr.open("PUT", `${baseUrl}/edit/${book.id}`);
+
+        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("X-Auth-Token", "12345");
+
+        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
+        xhr.send(JSON.stringify(book));
     };
 
     const removeBook = (bookId) => {
-        // tuliskan kode di sini!
+        // Membuat instance dari XMLHttpRequest
+        const xhr = new XMLHttpRequest();
+
+        //menetapkan callback jika response sukses dan error
+        xhr.onload = function() {
+            const responseJson = JSON.parse(this.responseText);
+            showResponseMessage(responseJson.message);
+            getBook();
+        };
+
+        xhr.onerror = function() {
+            showResponseMessage();
+        };
+
+        // Membuat DELETE request dan menetapkan target URL
+        xhr.open("DELETE", `${baseUrl}/delete/${bookId}`);
+
+        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
+        xhr.setRequestHeader("X-Auth-Token", "12345");
+
+        // Mengirimkan request
+        xhr.send();
     };
 
 
@@ -54,7 +142,7 @@ function main() {
     };
 
     const showResponseMessage = (message = "Check your internet connection") => {
-        alert("Check your internet connection");
+        alert(message);
     };
 
     document.addEventListener("DOMContentLoaded", () => {
