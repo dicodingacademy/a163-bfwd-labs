1114b8c0c20c7652e3a6df6f5124aa945b145e69
Dimas <dimas.maulanads@gmail.com>
2020-03-16 09:29:44 +0700

    Solution for search-bar

--- ClubFinder/src/script/component/search-bar.js
+++ ClubFinder/src/script/component/search-bar.js
@@ -1,4 +1,10 @@
 class SearchBar extends HTMLElement {
+
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
     connectedCallback(){
         this.render();
     }
@@ -10,18 +16,79 @@ class SearchBar extends HTMLElement {
 
 
     get value() {
-        return this.querySelector("#searchElement").value;
+        return this.shadowDOM.querySelector("#searchElement").value;
     }
 
     render() {
-        this.innerHTML = `
+        this.shadowDOM.innerHTML = `
+       <style>
+       .search-container {
+           max-width: 800px;
+           box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+           padding: 16px;
+           border-radius: 5px;
+           display: flex;
+           position: sticky;
+           top: 10px;
+           background-color: white;
+       }
+      
+       .search-container > input {
+           width: 75%;
+           padding: 16px;
+           border: 0;
+           border-bottom: 1px solid cornflowerblue;
+           font-weight: bold;
+       }
+      
+       .search-container > input:focus {
+           outline: 0;
+           border-bottom: 2px solid cornflowerblue;
+       }
+      
+       .search-container > input:focus::placeholder {
+           font-weight: bold;
+       }
+      
+       .search-container >  input::placeholder {
+           color: cornflowerblue;
+           font-weight: normal;
+       }
+      
+       .search-container > button {
+           width: 23%;
+           cursor: pointer;
+           margin-left: auto;
+           padding: 16px;
+           background-color: cornflowerblue;
+           color: white;
+           border: 0;
+           text-transform: uppercase;
+       }
+      
+       @media screen and (max-width: 550px){
+           .search-container {
+               flex-direction: column;
+               position: static;
+           }
+      
+           .search-container > input {
+               width: 100%;
+               margin-bottom: 12px;
+           }
+      
+           .search-container > button {
+               width: 100%;
+           }
+       }
+       </style>
        <div id="search-container" class="search-container">
            <input placeholder="Search football club" id="searchElement" type="search">
            <button id="searchButtonElement" type="submit">Search</button>
        </div>
        `;
 
-        this.querySelector("#searchButtonElement").addEventListener("click", this._clickEvent);
+        this.shadowDOM.querySelector("#searchButtonElement").addEventListener("click", this._clickEvent);
     }
 }
 
