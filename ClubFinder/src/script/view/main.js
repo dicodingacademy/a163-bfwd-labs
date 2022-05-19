719835c4bb3811801a0d5090465c77e21b81fac2
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-19 23:37:49 +0700

    Re-indent and Fix code convention

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,37 +1,38 @@
 const main = function () {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
-    const clubListElement = document.querySelector("#clubList");
+  const searchElement = document.querySelector('#searchElement');
+  const buttonSearchElement = document.querySelector('#searchButtonElement');
+  const clubListElement = document.querySelector('#clubList');
 
-    const onButtonSearchClicked = function () {
-        const dataSource = new DataSource(renderResult, fallbackResult);
-        dataSource.searchClub(searchElement.value);
-    };
+  const onButtonSearchClicked = function () {
+    const dataSource = new DataSource(renderResult, fallbackResult);
+    dataSource.searchClub(searchElement.value);
+  };
 
-    const renderResult = function (results) {
-        clubListElement.innerHTML = "";
-        results.forEach(function (club) {
-            const name = club.name;
-            const fanArt = club.fanArt;
-            const description = club.description;
+  const renderResult = function (results) {
+    clubListElement.innerHTML = '';
+    results.forEach(function (club) {
+      const name = club.name;
+      const fanArt = club.fanArt;
+      const description = club.description;
 
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
+      const clubElement = document.createElement('div');
+      clubElement.setAttribute('class', 'club');
 
-            clubElement.innerHTML = `
-                   <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-                   <div class="club-info">
-                       <h2>${name}</h2>
-                       <p>${description}</p>
-                   </div>`;
-            clubListElement.appendChild(clubElement);
-        })
-    };
+      clubElement.innerHTML = `
+        <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+        <div class="club-info">
+          <h2>${name}</h2>
+          <p>${description}</p>
+        </div>
+      `;
+      clubListElement.appendChild(clubElement);
+    });
+  };
 
-    const fallbackResult = function (message) {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
-    };
+  const fallbackResult = function (message) {
+    clubListElement.innerHTML = '';
+    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+  };
 
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+  buttonSearchElement.addEventListener('click', onButtonSearchClicked);
 };
