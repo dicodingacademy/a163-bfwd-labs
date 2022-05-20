71ded4946b1a8c679b3a2ae53957df6a4663c091
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-20 10:03:12 +0700

    Re-Indent and Fix code convention

--- ClubFinder/app.js
+++ ClubFinder/app.js
@@ -1,4 +1,4 @@
-import "./src/script/component/app-bar.js";
-import main from "./src/script/view/main.js";
+import './src/script/component/app-bar.js';
+import main from './src/script/view/main.js';
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,16 +1,17 @@
 import clubs from './clubs.js';
 
 class DataSource {
-    static searchClub(keyword) {
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
-            } else {
-                reject(`${keyword} is not found`);
-            }
-        });
-    }
+  static searchClub(keyword) {
+    return new Promise((resolve, reject) => {
+      const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+      
+      if (filteredClubs.length) {
+        resolve(filteredClubs);
+      } else {
+        reject(`${keyword} is not found`);
+      }
+    });
+  }
 }
 
 export default DataSource;
