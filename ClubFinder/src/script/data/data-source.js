2c5c8678974cbfbcf8c0ed9a7bf5d46e2ae2f89d
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-20 09:53:46 +0700

    Re-indent and fix code convention

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,12 +1,13 @@
 class DataSource {
-    static searchClub(keyword) {
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
-            } else {
-                reject(`${keyword} is not found`);
-            }
-        });
-    }
+  static searchClub(keyword) {
+    return new Promise((resolve, reject) => {
+      const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+      if (filteredClubs.length) {
+        resolve(filteredClubs);
+      } else {
+        reject(`${keyword} is not found`);
+      }
+    });
+  }
 }
--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,39 +1,41 @@
 const main = () => {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
-    const clubListElement = document.querySelector("#clubList");
+  const searchElement = document.querySelector('#searchElement');
+  const buttonSearchElement = document.querySelector('#searchButtonElement');
+  const clubListElement = document.querySelector('#clubList');
 
-    const onButtonSearchClicked = async () => {
-        try{
-            const result = await DataSource.searchClub(searchElement.value);
-            renderResult(result);
-        } catch (message) {
-            fallbackResult(message)
-        }
-    };
+  const onButtonSearchClicked = async () => {
+    try {
+      const result = await DataSource.searchClub(searchElement.value);
+      renderResult(result);
+    } catch (message) {
+      fallbackResult(message);
+    }
+  };
 
-    const renderResult =  results => {
-        clubListElement.innerHTML = "";
-        results.forEach(club => {
-            const {name, fanArt, description} = club;
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
+  const renderResult = results => {
+    clubListElement.innerHTML = '';
 
-            clubElement.innerHTML = `
-               <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-               <div class="club-info">
-                   <h2>${name}</h2>
-                   <p>${description}</p>
-               </div>`;
+    results.forEach(club => {
+      const {name, fanArt, description} = club;
+      const clubElement = document.createElement('div');
+      clubElement.setAttribute('class', 'club');
 
-            clubListElement.appendChild(clubElement);
-        })
-    };
+      clubElement.innerHTML = `
+        <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+        <div class="club-info">
+          <h2>${name}</h2>
+          <p>${description}</p>
+        </div>
+      `;
 
-    const fallbackResult = message => {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
-    };
+      clubListElement.appendChild(clubElement);
+    });
+  };
 
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+  const fallbackResult = message => {
+    clubListElement.innerHTML = '';
+    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+  };
+
+  buttonSearchElement.addEventListener('click', onButtonSearchClicked);
 };
