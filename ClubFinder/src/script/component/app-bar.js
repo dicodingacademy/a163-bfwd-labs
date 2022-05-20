836e7792f6c60973c65b9e6d7d1924e9d17dab85
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-20 10:14:33 +0700

    Re-Indent and Fix code convention

--- ClubFinder/src/script/component/app-bar.js
+++ ClubFinder/src/script/component/app-bar.js
@@ -1,35 +1,37 @@
 class AppBar extends HTMLElement {
 
-    constructor() {
-        super();
-        this.shadowDOM = this.attachShadow({mode: "open"});
-    }
+  constructor() {
+    super();
+    this.shadowDOM = this.attachShadow({mode: 'open'});
+  }
 
-    connectedCallback(){
-        this.render();
-    }
+  connectedCallback() {
+    this.render();
+  }
 
-    render() {
-        this.shadowDOM.innerHTML = `
-       <style>
-           * {
-               margin: 0;
-               padding: 0;
-               box-sizing: border-box;
-           }
-           :host {
-               display: block;
-               width: 100%;
-               background-color: cornflowerblue;
-               color: white;
-               box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-           }
-           h2 {
-               padding: 16px;
-           }
-       </style>
-       <h2>Club Finder</h2>`;
-    }
+  render() {
+    this.shadowDOM.innerHTML = `
+      <style>
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+        :host {
+          display: block;
+          width: 100%;
+          background-color: cornflowerblue;
+          color: white;
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        }
+        h2 {
+          padding: 16px;
+        }
+      </style>
+      
+      <h2>Club Finder</h2>
+    `;
+  }
 }
 
-customElements.define("app-bar", AppBar);
+customElements.define('app-bar', AppBar);
