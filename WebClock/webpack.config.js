1286219165158e925b00050f3cefebbfad4d2d9c
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-21 09:53:11 +0700

    Re-indent and Fix code convention

--- WebClock/webpack.config.js
+++ WebClock/webpack.config.js
@@ -1,48 +1,48 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    mode: "production",
-    module: {
-        rules: [
-            /* style and css loader */
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
-            },
-            /* babel loader */
-            {
-                test: /\.js$/,
-                exclude: "/node_modules/",
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"]
-                        }
-                    }
-                ]
-            }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      /* style and css loader */
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
         ]
-    },
-    /* plugin */
-    plugins: [
-        /* HTML Webpack Plugin */
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-            filename: "index.html"
-        })
+      },
+      /* babel loader */
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }
     ]
-}
+  },
+  /* plugin */
+  plugins: [
+    /* HTML Webpack Plugin */
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html'
+    })
+  ]
+};
