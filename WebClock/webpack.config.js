aba12358433cc68b4c2afd0154386f30395360dc
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-21 09:48:49 +0700

    Re-indent and Fix code convention

--- WebClock/webpack.config.js
+++ WebClock/webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    mode: "production"
-}
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'production'
+};
