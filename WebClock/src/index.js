9b2cbcfefe8cc2e7cf4e625b124eed4886c0a6fb
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-21 09:51:03 +0700

    Re-indent and Fix code convention

--- WebClock/src/index.js
+++ WebClock/src/index.js
@@ -1,16 +1,17 @@
-import "./style/style.css";
-import $ from "jquery";
-import moment from "moment";
+import './style/style.css';
+import $ from 'jquery';
+import moment from 'moment';
 
 const displayTime = () => {
-    moment.locale("id");
-    $(".time").text(moment().format("LTS"));
-    $(".date").text(moment().format("LL"));
+  moment.locale('id');
+
+  $('.time').text(moment().format('LTS'));
+  $('.date').text(moment().format('LL'));
 };
 
 const updateTime = () => {
-    displayTime();
-    setTimeout(updateTime, 1000)
+  displayTime();
+  setTimeout(updateTime, 1000);
 };
 
 updateTime();
--- WebClock/webpack.config.js
+++ WebClock/webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    mode: "production",
-    module: {
-        rules: [
-            /* style and css loader */
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
-            },
-            /* babel loader */
-            {
-                test: /\.js$/,
-                exclude: "/node_modules/",
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"]
-                        }
-                    }
-                ]
-            }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      /* style and css loader */
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
         ]
-    }
-}
+      },
+      /* babel loader */
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
