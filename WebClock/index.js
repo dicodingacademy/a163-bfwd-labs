2c7c8d942d5f5cd784be64c9fb42bbe8e9bef2db
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-21 09:44:52 +0700

    Re-indent and Fix code convention

--- WebClock/index.js
+++ WebClock/index.js
@@ -1,12 +1,12 @@
 const displayTime = () => {
-    moment.locale("id");
-    $(".time").text(moment().format("LTS"));
-    $(".date").text(moment().format("LL"));
+  moment.locale('id');
+  $('.time').text(moment().format('LTS'));
+  $('.date').text(moment().format('LL'));
 };
 
 const updateTime = () => {
-    displayTime();
-    setTimeout(updateTime, 1000)
+  displayTime();
+  setTimeout(updateTime, 1000);
 };
 
 updateTime();
