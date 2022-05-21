3a1704d2fad0d0e25913a3d78707dfae883413ab
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-21 10:42:11 +0700

    Re-indent and Fix code convention

--- as-a-module/webpack.config.js
+++ as-a-module/webpack.config.js
@@ -1,24 +1,23 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: "file-loader"
-                    }
-                ]
-            }
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
         ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: path.resolve(__dirname, 'dist/index.html'),
-            template: path.resolve(__dirname, 'src/template.html')
-        })
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/template.html')
+    })
+  ]
 };
--- in-html-usage/webpack.config.js
+++ in-html-usage/webpack.config.js
@@ -1,21 +1,20 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: path.resolve(__dirname, 'dist/index.html'),
-            template: path.resolve(__dirname, 'src/template.html')
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/images'),
-                    to: path.resolve(__dirname, 'dist/images')
-                }
-            ]
-        })
-
-    ]
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/template.html')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/images'),
+          to: path.resolve(__dirname, 'dist/images')
+        }
+      ]
+    })
+  ]
 };
