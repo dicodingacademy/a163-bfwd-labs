265d6accf053fd17cd23430891cf51c74cd39600
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-21 10:06:12 +0700

    Re-indent and Fix code convention

--- DicodingBooks/src/scripts/main.js
+++ DicodingBooks/src/scripts/main.js
@@ -1,90 +1,90 @@
 function main() {
 
-    const getBook = () => {
-        // tuliskan kode di sini!
-    };
+  const getBook = () => {
+    // tuliskan kode di sini!
+  };
 
+  const insertBook = (book) => {
+    // tuliskan kode di sini!
+  };
 
-    const insertBook = (book) => {
-        // tuliskan kode di sini!
-    };
+  const updateBook = (book) => {
+    // tuliskan kode di sini!
+  };
 
-    const updateBook = (book) => {
-        // tuliskan kode di sini!
-    };
-
-    const removeBook = (bookId) => {
-        // tuliskan kode di sini!
-    };
-
+  const removeBook = (bookId) => {
+    // tuliskan kode di sini!
+  };
 
 
 
 
 
-    /*
-        jangan ubah kode di bawah ini ya!
-    */
+  /*
+      jangan ubah kode di bawah ini ya!
+  */
 
-    const renderAllBooks = (books) => {
-        const listBookElement = document.querySelector("#listBook");
-        listBookElement.innerHTML = "";
+  const renderAllBooks = (books) => {
+    const listBookElement = document.querySelector('#listBook');
+    listBookElement.innerHTML = '';
 
-        books.forEach(book => {
-            listBookElement.innerHTML += `
-                <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5>(${book.id}) ${book.title}</h5>
-                            <p>${book.author}</p>
-                            <button type="button" class="btn btn-danger button-delete" id="${book.id}">Hapus</button>
-                        </div>
-                    </div>
-                </div>
-            `;
-        });
-
-        const buttons = document.querySelectorAll(".button-delete");
-        buttons.forEach(button => {
-            button.addEventListener("click", event => {
-                const bookId = event.target.id;
-                removeBook(bookId);
-            })
-        })
-    };
-
-    const showResponseMessage = (message = "Check your internet connection") => {
-        alert(message);
-    };
-
-    document.addEventListener("DOMContentLoaded", () => {
-
-        const inputBookId = document.querySelector("#inputBookId");
-        const inputBookTitle = document.querySelector("#inputBookTitle");
-        const inputBookAuthor = document.querySelector("#inputBookAuthor");
-        const buttonSave = document.querySelector("#buttonSave");
-        const buttonUpdate = document.querySelector("#buttonUpdate");
-
-        buttonSave.addEventListener("click", function () {
-            const book = {
-                id: Number.parseInt(inputBookId.value),
-                title: inputBookTitle.value,
-                author: inputBookAuthor.value
-            };
-            insertBook(book)
-        });
-
-        buttonUpdate.addEventListener("click", function () {
-            const book = {
-                id: Number.parseInt(inputBookId.value),
-                title: inputBookTitle.value,
-                author: inputBookAuthor.value
-            };
-
-            updateBook(book)
-        });
-        getBook();
+    books.forEach(book => {
+      listBookElement.innerHTML += `
+        <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
+          <div class="card">
+            <div class="card-body">
+              <h5>(${book.id}) ${book.title}</h5>
+              <p>${book.author}</p>
+              <button type="button" class="btn btn-danger button-delete" id="${book.id}">Hapus</button>
+            </div>
+          </div>
+        </div>
+      `;
     });
+
+    const buttons = document.querySelectorAll(".button-delete");
+    buttons.forEach(button => {
+      button.addEventListener('click', event => {
+        const bookId = event.target.id;
+        removeBook(bookId);
+      });
+    });
+  };
+
+  const showResponseMessage = (message = 'Check your internet connection') => {
+    alert(message);
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const inputBookId = document.querySelector('#inputBookId');
+    const inputBookTitle = document.querySelector('#inputBookTitle');
+    const inputBookAuthor = document.querySelector('#inputBookAuthor');
+    const buttonSave = document.querySelector('#buttonSave');
+    const buttonUpdate = document.querySelector('#buttonUpdate');
+
+    buttonSave.addEventListener('click', function () {
+      const book = {
+        id: Number.parseInt(inputBookId.value),
+        title: inputBookTitle.value,
+        author: inputBookAuthor.value
+      };
+
+      insertBook(book)
+    });
+
+    buttonUpdate.addEventListener('click', function () {
+      const book = {
+        id: Number.parseInt(inputBookId.value),
+        title: inputBookTitle.value,
+        author: inputBookAuthor.value
+      };
+
+      updateBook(book);
+    });
+
+    getBook();
+  });
 }
 
 export default main;
