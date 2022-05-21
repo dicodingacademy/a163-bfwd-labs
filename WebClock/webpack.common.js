bead2272ffe000e41decb621322c28038237c610
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-05-21 09:56:29 +0700

    Re-indent and Fix code convention

--- WebClock/webpack.common.js
+++ WebClock/webpack.common.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            /* style and css loader */
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
-            }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      /* style and css loader */
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
         ]
-    },
-    /* plugin */
-    plugins: [
-        /* HTML Webpack Plugin */
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-            filename: "index.html"
-        })
+      }
     ]
-}
+  },
+  /* plugin */
+  plugins: [
+    /* HTML Webpack Plugin */
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html'
+    })
+  ]
+};
