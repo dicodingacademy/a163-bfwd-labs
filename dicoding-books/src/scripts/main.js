d47d1138a3e566f55e9a87b407b0c5b0e524c7bd
Nur Rizki Adi Prasetyo <nrizki@dicoding.com>
2023-11-05 13:52:04 +0700

    Add gitignore and update version packages
    Another updates are operational code, webpack config(s), etc.

--- dicoding-books/src/scripts/main.js
+++ dicoding-books/src/scripts/main.js
@@ -1,5 +1,4 @@
 function main() {
-
   const baseUrl = 'https://books-api.dicoding.dev';
 
   const getBook = async () => {
@@ -22,17 +21,17 @@ function main() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'X-Auth-Token': '12345'
+          'X-Auth-Token': '12345',
         },
-        body: JSON.stringify(book)
+        body: JSON.stringify(book),
       };
 
-      const response = await fetch(`${baseUrl}/add`, options)
+      const response = await fetch(`${baseUrl}/add`, options);
       const responseJson = await response.json();
       showResponseMessage(responseJson.message);
       getBook();
     } catch (error) {
-      showResponseMessage(error)
+      showResponseMessage(error);
     }
   };
 
@@ -42,9 +41,9 @@ function main() {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
-          'X-Auth-Token': '12345'
+          'X-Auth-Token': '12345',
         },
-        body: JSON.stringify(book)
+        body: JSON.stringify(book),
       };
 
       const response = await fetch(`${baseUrl}/edit/${book.id}`, options);
@@ -61,51 +60,54 @@ function main() {
     fetch(`${baseUrl}/delete/${bookId}`, {
       method: 'DELETE',
       headers: {
-        'X-Auth-Token': '12345'
-      }
+        'X-Auth-Token': '12345',
+      },
     })
-        .then(response => {
-          return response.json();
-        })
-        .then(responseJson => {
-          showResponseMessage(responseJson.message);
-          getBook();
-        })
-        .catch(error => {
-          showResponseMessage(error);
-        });
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJson) => {
+        showResponseMessage(responseJson.message);
+        getBook();
+      })
+      .catch((error) => {
+        showResponseMessage(error);
+      });
   };
 
-
-
-
-
   /*
-    jangan ubah kode di bawah ini ya!
+    PERINGATAN: Jangan ubah kode di bawah ini!
   */
 
   const renderAllBooks = (books) => {
     const listBookElement = document.querySelector('#listBook');
     listBookElement.innerHTML = '';
 
-    books.forEach(book => {
+    books.forEach((book) => {
       listBookElement.innerHTML += `
         <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
           <div class="card">
             <div class="card-body">
               <h5>(${book.id}) ${book.title}</h5>
               <p>${book.author}</p>
-              <button type="button" class="btn btn-danger button-delete" id="${book.id}">Hapus</button>
+              <button
+                type="button"
+                class="btn btn-danger btn-delete"
+                data-id="${book.id}"
+              >
+                Hapus
+              </button>
             </div>
           </div>
         </div>
       `;
     });
 
-    const buttons = document.querySelectorAll('.button-delete');
-    buttons.forEach(button => {
-      button.addEventListener('click', event => {
-        const bookId = event.target.id;
+    const deleteButtonElements = document.querySelectorAll('.btn-delete');
+    deleteButtonElements.forEach((button) => {
+      button.addEventListener('click', (event) => {
+        const bookId = event.target.dataset.id;
+
         removeBook(bookId);
       });
     });
@@ -116,31 +118,30 @@ function main() {
   };
 
   document.addEventListener('DOMContentLoaded', () => {
+    const bookForm = document.querySelector('form');
 
-    const inputBookId = document.querySelector('#inputBookId');
-    const inputBookTitle = document.querySelector('#inputBookTitle');
-    const inputBookAuthor = document.querySelector('#inputBookAuthor');
-    const buttonSave = document.querySelector('#buttonSave');
-    const buttonUpdate = document.querySelector('#buttonUpdate');
+    const inputBookId = bookForm.elements.inputBookId;
+    const inputBookTitle = bookForm.elements.inputBookTitle;
+    const inputBookAuthor = bookForm.elements.inputBookAuthor;
 
-    buttonSave.addEventListener('click', function () {
+    bookForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+
       const book = {
         id: Number.parseInt(inputBookId.value),
         title: inputBookTitle.value,
-        author: inputBookAuthor.value
+        author: inputBookAuthor.value,
       };
 
-      insertBook(book);
-    });
+      switch (event.submitter.textContent) {
+        case 'Save':
+          insertBook(book);
+          break;
 
-    buttonUpdate.addEventListener('click', function () {
-      const book = {
-        id: Number.parseInt(inputBookId.value),
-        title: inputBookTitle.value,
-        author: inputBookAuthor.value
-      };
-
-      updateBook(book);
+        case 'Update':
+          updateBook(book);
+          break;
+      }
     });
 
     getBook();
